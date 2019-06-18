02053fcba398794c776efbec86b92b79a8c8262d
samuel.vaz <samuel.rvg@gmail.com>
2019-06-17 22:29:29 -0400

    playground

---
+++ curso-ios.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+var str = "Hello, playground"
+
+5+4
+let sa = "samuel renan"
+
+var sobrenome = "renan"
+
+var idade: Int  = 1
+
+var dec : Decimal
+
+dec = 9.43
+
+print("o valor é", dec)
+
+var nomes = ["samuel", "renan", "davi"]
+
+print(nomes[2])
+
+var arrStr : [String]
+var arrInt: [Int]
+
+arrStr = ["Samuel"]
+arrStr += ["Vaz"]
+
+arrStr.remove(at: 0)
+
+arrStr.append("Muka")
