14e4ad2dac5d7fe805d30daceaca20de85d0d5d0
samuel.vaz <samuel.rvg@gmail.com>
2019-06-28 22:57:45 -0400

    ajust layout  app

--- Alcool ou Gasolina/Alcool ou Gasolina/ViewController.swift
+++ Alcool ou Gasolina/Alcool ou Gasolina/ViewController.swift
@@ -10,11 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var precoGasolinaCampo: UITextField!
+    @IBOutlet weak var precoAlcoolCampo: UITextField!
+    @IBOutlet weak var resultado: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calcularMelhorPreco(_ sender: Any) {
+        if let precoAlcool = self.precoAlcoolCampo.text{
+            if let precoGasolina = self.precoGasolinaCampo.text{
+                let validaCampos = self.validarCampos(precoAlcool: precoAlcool, precoGasolina: precoGasolina)
+                
+                if validaCampos{
+                    self.calculaPreco(precoAlcool: precoAlcool, precoGasolina: precoGasolina)
+                }
+                else{
+                    self.resultado.text = "Preencha os campos!"
+                }
+            }
+        }
+    }
+    
+    func calculaPreco(precoAlcool: String, precoGasolina: String){
+        self.resultado.text = ""
+        if let valorAlcool = Double(precoAlcool){
+            if let valorGasolina = Double(precoGasolina){
+                let resultado = valorAlcool / valorGasolina
+                if resultado >= 0.7{
+                    self.resultado.text = "É melhor usar gasolina!"
+                }
+                else{
+                    self.resultado.text = "É melhor usar alcool!"
+                }
+            }
+        }
+    }
+    
+    func validarCampos(precoAlcool: String, precoGasolina: String) -> Bool{
+        
+        var camposValidos = true
+        
+        if precoAlcool.isEmpty{
+            camposValidos = false
+        }
+        if precoGasolina.isEmpty {
+            camposValidos = false
+        }
+        
+        return camposValidos
+    }
+    
+    
 }
 
